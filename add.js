791afe5fe9b070a10396ee4edeef4bf0b8e8423e
saiteja'git <tejas5926@gmail.com>
2023-10-17 16:59:47 +0530

    first commit

---
+++ add.js
@@ -0,0 +1,15 @@
+function add(a,b){
+    var a;
+    var b;
+    result= a+b;
+    console.log("result is "+result)
+}
+
+function remove(a,b){
+    var a;
+    var b;
+    result= a+b;
+    console.log("result is "+result)
+}
+add(20,30)
+remove(20,30)
